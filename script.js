4358764505b001c191fad3dc171a9ba8b2efd31a
Radek <radolo77@gmail.com>
2023-08-18 21:33:36 +0100

    Small changes

--- script.js
+++ script.js
@@ -1,56 +1,44 @@
-const questions = [
-        {
-            "question": "Which Country won the first-ever FIFA World Cup in 1930?",
-            "answers": ["A. Brazil", "B. Germany", "C. Uruguay", "D. France"],
-            "correctIndex": 2
-        },
-        {
-            "question": "What is the name of the FC Barcelona stadium?",
-            "answers": ["A. Santiago Bernabeu", "B. Camp Nou", "C. Allianz Arena", "D. San Siro"],
-            "correctIndex": 1
-        },
-        {
-            "question": "Who is the all-time top scorer in men's international football?",
-            "answers": ["A. Lionel Messi", "B. Thierry Henry", "C. Cristiano Ronaldo", "D. Robert Lewandowski"],
-            "correctIndex": 2
-        },
-        {
-            "question": "The \"Hand of God\" goal is associated with which legendary player?",
-            "answers": ["A. Diego Maradona", "B. Pele", "C. Zinedine Zidane", "D. Johan Cruyff"],
-            "correctIndex": 0
-        },
-        {
-            "question": "Which player is famously referred to as \"The King\" in the football world?",
-            "answers": ["A. Eric Cantona", "B. Pele", "C. Robbie Fowler", "D. Diego Maradona"],
-            "correctIndex": 1
-        },
-        {
-            "question": "How long is a standard game of football?",
-            "answers": ["A. 70 minutes", "B. 80 minutes", "C. 90 minutes", "D. 100 minutes"],
-            "correctIndex": 2
-        },
-        {
-            "question": "Which country has won the most FIFA World Cup titles as of 2023?",
-            "answers": ["A. Germany", "B. Argentina", "C. Italy", "D. Brazil"],
-            "correctIndex": 3
-        },
-        {
-            "question": "Which football club is owned by former English footballer David Beckham?",
-            "answers": ["A. Inter Miami CF", "B. Manchester United", "C. LA Galaxy", "D. Wrexham AFC"],
-            "correctIndex": 0
-        },
-        {
-            "question": "Who is the all-time Premier League Top Scorer?",
-            "answers": ["A. Wayne Rooney", "B. Alan Shearer", "C. Cristiano Ronaldo", "D. Harry Kane"],
-            "correctIndex": 1
-        },
-        {
-            "question": "Which of the following teams have NOT won the UEFA Champions League?",
-            "answers": ["A. Arsenal", "B. Celtic", "C. Aston Villa", "D. PSV Eindhoven"],
-            "correctIndex": 0
-        }
-    ]
-;
+const questions = [{
+    "question": "Which Country won the first-ever FIFA World Cup in 1930?",
+    "answers": ["A. Brazil", "B. Germany", "C. Uruguay", "D. France"],
+    "correctIndex": 2
+}, {
+    "question": "What is the name of the FC Barcelona stadium?",
+    "answers": ["A. Santiago Bernabeu", "B. Camp Nou", "C. Allianz Arena", "D. San Siro"],
+    "correctIndex": 1
+}, {
+    "question": "Who is the all-time top scorer in men's international football?",
+    "answers": ["A. Lionel Messi", "B. Thierry Henry", "C. Cristiano Ronaldo", "D. Robert Lewandowski"],
+    "correctIndex": 2
+}, {
+    "question": "The \"Hand of God\" goal is associated with which legendary player?",
+    "answers": ["A. Diego Maradona", "B. Pele", "C. Zinedine Zidane", "D. Johan Cruyff"],
+    "correctIndex": 0
+}, {
+    "question": "Which player is famously referred to as \"The King\" in the football world?",
+    "answers": ["A. Eric Cantona", "B. Pele", "C. Robbie Fowler", "D. Diego Maradona"],
+    "correctIndex": 1
+}, {
+    "question": "How long is a standard game of football?",
+    "answers": ["A. 70 minutes", "B. 80 minutes", "C. 90 minutes", "D. 100 minutes"],
+    "correctIndex": 2
+}, {
+    "question": "Which country has won the most FIFA World Cup titles as of 2023?",
+    "answers": ["A. Germany", "B. Argentina", "C. Italy", "D. Brazil"],
+    "correctIndex": 3
+}, {
+    "question": "Which football club is owned by former English footballer David Beckham?",
+    "answers": ["A. Inter Miami CF", "B. Manchester United", "C. LA Galaxy", "D. Wrexham AFC"],
+    "correctIndex": 0
+}, {
+    "question": "Who is the all-time Premier League Top Scorer?",
+    "answers": ["A. Wayne Rooney", "B. Alan Shearer", "C. Cristiano Ronaldo", "D. Harry Kane"],
+    "correctIndex": 1
+}, {
+    "question": "Which of the following teams have NOT won the UEFA Champions League?",
+    "answers": ["A. Arsenal", "B. Celtic", "C. Aston Villa", "D. PSV Eindhoven"],
+    "correctIndex": 0
+}];
 
 let currentQuestionIndex = 0;
 let score = 0;
@@ -138,7 +126,6 @@ answerButtons.forEach(button => {
 
 const quizContainer = document.getElementById("quiz");
 const aboutContainer = document.getElementById("about");
-const contactContainer = document.getElementById("contact");
 
 function resetQuiz() {
     currentQuestionIndex = 0;
@@ -151,7 +138,6 @@ function resetQuiz() {
 
     quizContainer.style.display = "block";
     aboutContainer.style.display = "none";
-    contactContainer.style.display = "none";
 
     showQuestion();
 }
@@ -159,7 +145,6 @@ function resetQuiz() {
 document.addEventListener("DOMContentLoaded", () => {
     const homeLink = document.getElementById("home-link");
     const aboutLink = document.getElementById("about-link");
-    const contactLink = document.getElementById("contact-link");
 
     homeLink.addEventListener("click", (event) => {
         event.preventDefault();
@@ -169,16 +154,6 @@ document.addEventListener("DOMContentLoaded", () => {
     aboutLink.addEventListener("click", (event) => {
         event.preventDefault();
         quizContainer.style.display = "none";
-        contactContainer.style.display = "none";
         aboutContainer.style.display = "block";
     });
-
-    contactLink.addEventListener("click", (event) => {
-        event.preventDefault();
-        quizContainer.style.display = "none";
-        aboutContainer.style.display = "none";
-        contactContainer.style.display = "block";
-    });
 });
-
-showQuestion();
